c5615eea8dc9d32891acb8338e23612f35107ef4
Blank <mbrient@ville-clermont-ferrand.fr>
2016-07-25 09:11:22 +0200

    Update for glpi 9.1

--- setup.php
+++ setup.php
@@ -98,7 +98,7 @@ function plugin_init_treeview() {
 function plugin_version_treeview() {
 
    return array('name'           => __('Tree view', 'treeview'),
-                'version'        => '1.8.6',
+                'version'        => '1.8.7',
                 'license'        => 'GPLv2+',
                 'author'         => 'AL-Rubeiy Hussein, Xavier Caillaud, Nelly Mahu-Lasson',
                 'homepage'       => 'https://forge.indepnet.net/projects/treeview',
@@ -108,8 +108,8 @@ function plugin_version_treeview() {
 
 function plugin_treeview_check_prerequisites() {
 
-   if (version_compare(GLPI_VERSION,'0.85','lt') || version_compare(GLPI_VERSION,'0.91','gt')) {
-      echo "This plugin requires GLPI >= 0.85 and GLPI < 0.90.1";
+   if (version_compare(GLPI_VERSION,'0.85','lt') || version_compare(GLPI_VERSION,'9.1','gt')) {
+      echo "This plugin requires GLPI >= 0.85 and GLPI < 9.1";
       return false;
    }
    return true;
